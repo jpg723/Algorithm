ea25d490ef9d67b9d61c6f0652f056a40c050c99
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-10-27 10:11:00 +0900

    [SILVER]BJ24418

--- java/BAEKJOON/src/Silver/Silver4/BJ24418.java
+++ java/BAEKJOON/src/Silver/Silver4/BJ24418.java
@@ -1,53 +1,65 @@
 package Silver.Silver4;
 
-
-import java.util.Scanner;
-
-import static java.lang.Math.max;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class BJ24418 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int m[][];
+    static int cnt1 = 0, cnt2 = 0;
 
-        int n = sc.nextInt();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] m = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                m[i][j] = sc.nextInt();
+        m = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                m[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.println(matrix_path2(m, n));
+        matrix_path(m, n);
+        matrix_path2(m, n);
+
+        System.out.println(cnt1 + " " + cnt2);
     }
 
-    public static int matrix_path(int[][] m, int n, int k) {  // (1, 1)에서 (n, n)에 이르는 최고 점수를 구한다.
-        return matrix_path_rec(m, n, k);
-    }
-    public static int matrix_path_rec(int[][] m, int i, int j) {  // (1, 1)에서 (i, j)에 이르는 최고 점수를 구한다.
-        if (i == 0 || j == 0)
+    static int matrix_path_rec(int[][] m, int i, int j) {
+        // (1, 1) ~ (i, j)의 최고 점수
+        if(i == 0 || j == 0) {
+            cnt1++;
             return 0;
-        else
-            return (m[i][j] + (max(matrix_path(m, i-1, j), matrix_path(m, i, j-1))));
+        }
+
+        else {
+            return m[i][j] + Math.max(matrix_path_rec(m, i-1, j), matrix_path_rec(m, i, j-1));
+        }
+    }
+    static int matrix_path(int[][] m, int n) {
+        return matrix_path_rec(m, n, n); // (1, 1) ~ (n, n)의 최고 점수;
     }
 
-    //행렬 경로 문제 동적 프로그래밍
-    public static int matrix_path2(int m[][], int n) {  // (1, 1)에서 (n, n)에 이르는 최고 점수를 구한다.
-        int[][]d = new int[n + 1][n + 1];
+    static int matrix_path2(int[][] m, int n) {
+        int[][] d = new int [n + 1][n + 1];
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++)
             d[i][0] = 0;
-        }
-        for (int j = 1; j <= n; j++) {
+
+        for (int j = 0; j < n; j++)
             d[0][j] = 0;
-        }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                d[i][j] = m[i][j] + max(d[i - 1][j], d[i][j - 1]);
+                cnt2++;
+                d[i][j] = m[i][j] + Math.max(d[i - 1][j], d[i][j - 1]);
             }
         }
         return d[n][n];
     }
+
 
 }
