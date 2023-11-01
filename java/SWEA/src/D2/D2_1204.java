8c3dae3192ad79f9eec6980aa2d84aa28e3704b9
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-11-02 02:21:58 +0900

    [SWEA]1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기

--- java/SWEA/src/D2/D2_1204.java
+++ java/SWEA/src/D2/D2_1204.java
@@ -3,9 +3,13 @@ package D2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import static sun.swing.MenuItemLayoutHelper.max;
+
 public class D2_1204 {
+    /*https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV13zo1KAAACFAYh&categoryId=AV13zo1KAAACFAYh&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1*/
     public static void main(String args[]) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,27 +18,31 @@ public class D2_1204 {
 
         for(int i = 0; i < T; i++){
             int testcase_num = Integer.parseInt(br.readLine());
-            int[] score = new int[5];
-            int[] count_max = new int[5];
+            int[] score = new int[1000];
+            int[] count_max = new int[101];
 
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            for(int j = 0; j < 5; j++){
+            //점수 입력 받기
+            for(int j = 0; j < 1000; j++){
                 score[j] = Integer.parseInt(st.nextToken());
             }
 
-            for(int j = 0; j < 5; j++){
-                for(int k = 0; k < 5; k++){
-                    if(score[j] == score[k])
-                        count_max[j]++;
+            for(int j = 0; j < 1000; j++){
+                count_max[score[j]] += 1;
+            }
+
+            int max = 0;
+            int index = 0;
+            for(int j = 0; j < 100; j++){
+                if(max <= count_max[j]) {
+                    max = count_max[j];
+                    index = j;
                 }
             }
 
-            for(int j = 0; j < 5; j++){
-                System.out.println(score[j] + ": " + count_max[j]);
-            }
-
-
+            System.out.println("#" + testcase_num + " " + index);
         }
     }
+
 }
