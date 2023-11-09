291abd370ec56f8649acc4940e0d1272ad236c62
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-11-09 13:15:31 +0900

    [GOLD4]BJ2580-스토쿠

--- java/BAEKJOON/src/백트래킹/GOLD4_BJ9663.java
+++ java/BAEKJOON/src/백트래킹/GOLD4_BJ9663.java
@@ -9,7 +9,9 @@ public class GOLD4_BJ9663 {
     * */
 
     public static int[] arr;
-    public static void main(String args[]) throws IOException{
+    public static int count = 0;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
@@ -17,11 +19,9 @@ public class GOLD4_BJ9663 {
         arr = new int[N];
 
         dfs(N, 0);
-        bw.write(count);
+        bw.write(String.valueOf(count));
         bw.flush();
     }
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static int count = 0;
 
     public static void dfs(int n, int depth) throws IOException{
 
@@ -43,20 +43,13 @@ public class GOLD4_BJ9663 {
     public static boolean Possibility(int col) {
 
         for (int i = 0; i < col; i++) {
-            // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
             if (arr[col] == arr[i]) {
                 return false;
             }
-
-            /*
-             * 대각선상에 놓여있는 경우
-             * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
-             */
             else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
                 return false;
             }
         }
-
         return true;
     }
 }
