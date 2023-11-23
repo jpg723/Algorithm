1c116be797e39d108b958659674fbb76e0e75c31
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-11-23 16:43:02 +0900

    [SLIVER3]BJ24060_알고리즘 수업-병합정렬1

--- java/BAEKJOON/src/재귀/SLIVER3_BJ24060.java
+++ java/BAEKJOON/src/재귀/SLIVER3_BJ24060.java
@@ -9,25 +9,33 @@ public class SLIVER3_BJ24060 {
     /*https://www.acmicpc.net/problem/24060
     알고리즘 수업-병합정렬1
      */
+
+    static int[] tmp;
+    static int cnt = 0; //저장횟수
+    static int K;
+    static int result = 0;
     public static void main(String args[])throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         //N과 K입력
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         //배열 A입력
         int[] A = new int[N];
+        tmp = new int[N];
 
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < N; i++)
             A[i] = Integer.parseInt(st.nextToken());
 
-        merge_sort(A, 0, N);
+        merge_sort(A, 0, (N - 1));
 
-        for(int i = 0; i < N; i++)
-            System.out.println(A[i]);
+        if(cnt < K)
+            System.out.println(-1);
+        else
+            System.out.println(result);
     }
 
     public static void merge_sort(int A[], int p, int r) { // A[p..r]을 오름차순 정렬한다.
@@ -44,8 +52,7 @@ public class SLIVER3_BJ24060 {
     public static void merge(int A[], int p, int q, int r) {
         int i = p;
         int j = q + 1;
-        int t = 1;
-        int[] tmp = new int[i + j];
+        int t = 0;
 
         while (i <= q && j <= r) {
             if (A[i] <= A[j])
@@ -53,14 +60,22 @@ public class SLIVER3_BJ24060 {
             else
                 tmp[t++] = A[j++]; //tmp[t] <- A[j]; t++; j++;
         }
-        while (i <= q)  // 왼쪽 배열 부분이 남은 경우
+        while (i <= q) { // 왼쪽 배열 부분이 남은 경우
             tmp[t++] = A[i++];
-
-        while (j <= r)  // 오른쪽 배열 부분이 남은 경우
+        }
+        while (j <= r) {  // 오른쪽 배열 부분이 남은 경우
             tmp[t++] = A[j++];
+        }
         i = p;
-        t = 1;
-        while (i <= r)  // 결과를 A[p..r]에 저장
-            A[i++] = tmp[t++];
+        t = 0;
+        while (i <= r){  // 결과를 A[p..r]에 저장
+            A[i] = tmp[t];
+            cnt++; //저장 후 저장횟수 증가
+            if(K == cnt) {
+                result = A[i];
+            }
+            i++;
+            t++;
+        }
     }
 }
