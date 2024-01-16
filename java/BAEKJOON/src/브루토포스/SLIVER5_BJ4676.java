63d742f9adf60c20ed0432e5c39cc13172ef393c
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-01-16 15:07:33 +0900

    [SLIVER5]BJ4676-셀프넘버

--- java/BAEKJOON/src/브루토포스/SLIVER5_BJ4676.java
+++ java/BAEKJOON/src/브루토포스/SLIVER5_BJ4676.java
@@ -3,6 +3,7 @@ package 브루토포스;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class SLIVER5_BJ4676 {
     /*
@@ -11,16 +12,33 @@ public class SLIVER5_BJ4676 {
      */
     public static void main(String[] args)throws IOException{
 
-        for(int i = 10; i <= 10000; i++){
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i = 1; i <= 10000; i++){
             String num = String.valueOf(i);
             int sum = 0;
             for(int j = 0; j < num.length(); j++){
                 char charNum = num.charAt(j);
-
                 int intNum = Integer.parseInt(String.valueOf(charNum));
+                //System.out.print(intNum + " + ");
                 sum += intNum;
             }
-            System.out.println(sum);
+            arr.add(sum + i);
+            //System.out.print(" + " + i + " = " + (sum + i) + "\n");
+        }
+
+        for(int i = 1; i <= 10000; i++){
+            boolean check = false;
+
+            for(int j = 0; j < 10000; j++) {
+                if (arr.get(j) == i)
+                    check = true;
+            }
+
+            if(check)
+                continue;
+            else
+                System.out.println(i);
         }
     }
 }
