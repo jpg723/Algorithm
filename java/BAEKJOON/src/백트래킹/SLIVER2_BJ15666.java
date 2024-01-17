2897fe0fe541dccb9da8ceb727894ad45b2a4435
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-01-17 18:58:17 +0900

    [SLIVER2] BJ15666-N과M(12)

---
+++ java/BAEKJOON/src/백트래킹/SLIVER2_BJ15666.java
@@ -0,0 +1,75 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SLIVER2_BJ15666 {
+    /*
+        https://www.acmicpc.net/problem/15666
+        N과 M(12)
+     */
+
+    static int[] arr;
+    static int[] result;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(N, M, 0);
+
+        System.out.print(sb);
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int n, int m, int depth){
+
+        if(m == depth){
+            int temp = 0;
+            boolean check = false;
+
+            for(int var: result){
+
+                if(temp > var)
+                    check = true;
+
+                temp = var;
+            }
+
+            if(check == false){
+                for(int var: result){
+                    sb.append(var + " ");
+                }
+                sb.append("\n");
+            }
+        }
+
+        else{
+            int before = 0;
+
+            for(int i = 0; i < n; i++){
+                if(before != arr[i]) {
+                    before = arr[i];
+                    result[depth] = arr[i];
+                    dfs(n, m, depth + 1);
+                }
+            }
+        }
+    }
+}
