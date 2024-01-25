343ed90597f24e0526680c33963c47c476cfd1ff
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-01-25 21:37:09 +0900

    [SLIVER1]BJ1931-회의실배정

---
+++ java/BAEKJOON/src/Greedy/SLIVER1_BJ1931.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SLIVER1_BJ1931 {
+    /*
+        https://www.acmicpc.net/problem/1931
+        회의실 배정
+     */
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //N입력
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+
+        }
+    }
+}
