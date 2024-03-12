ae51d0435f7ff80fee06f730f2c1a5a539b10d5d
MiJeeLee <dlalwl723@naver.com>
2024-03-13 02:37:02 +0900

    KT-CodingMarsters

--- java/KT-AIVLE-CodingMasters/src/초급/cm_8517.java
+++ java/KT-AIVLE-CodingMasters/src/초급/cm_8517.java
@@ -3,6 +3,8 @@ package 초급;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class cm_8517 {
@@ -13,11 +15,48 @@ public class cm_8517 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        //a, b입력
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
+        int[] fibofibo = new int[100];
+        int index = 0;
+        int num = 1;
+
+        // 피보나치 피보나치 수열 만들기
+        while(true){
+            int temp = fibo(num);
+
+            for(int i = 0; i < temp; i++){
+                fibofibo[index] = temp;
+                index++;
+
+                if(index == 100)
+                    break;
+            }
+
+            if(index == 100)
+                break;
+
+            num++;
+        }
+
+        int sum = 0;
+
+        for(int i = a; i <= b; i++){
+            sum += fibofibo[i - 1];
+        }
+
+        System.out.println(sum);
 
     }
 
+    public static int fibo(int n){
 
+        if(n == 1 || n == 2)
+            return 1;
+
+        else
+            return fibo(n - 2) + fibo(n - 1);
+    }
 }
