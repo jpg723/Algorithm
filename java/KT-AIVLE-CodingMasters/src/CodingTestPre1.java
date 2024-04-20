7a15a4445adafc76253e74865307880cf2dbed35
MiJeeLee <dlalwl723@naver.com>
2024-04-21 02:28:47 +0900

    [KT-AIVLE] 코테 연습용

---
+++ java/KT-AIVLE-CodingMasters/src/CodingTestPre1.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodingTestPre1 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 문자열 입력
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        // 알파벳 배열 생성
+        char[] alp = new char[26];
+        for(int i = 0; i < 26; i++){
+            alp[i] = (char)('A' + i);
+        }
+
+        // 문자열 시작
+        char start = str.charAt(0);
+        // 동일한 문자를 세는 cnt배열 변수
+        int[] cnt = new int[str.length()];
+        // 첫 번째로 시작할 cnt값(초기값)
+        cnt[0] = 1;
+        // cnt배열의 index값
+        int index = 0;
+        // 동일한 문자를 세는 반복문
+        for(int i = 1; i < str.length(); i++){
+
+            char c = str.charAt(i);
+
+            if(start != c){
+                index++;
+                cnt[index] = 1;
+                start = c;
+            }
+
+            else{
+                cnt[index]++;
+            }
+        }
+
+        // 결과 출력
+        StringBuilder result= new StringBuilder();
+        result.append(start);
+        for(int i = 0; i < index + 1; i++){
+            for(int j = 0; j < alp.length; j++){
+                if(cnt[i] == j + 1){
+                    result.append(alp[j]);
+                }
+            }
+        }
+
+
+        return result.toString();
+    }
+}
+
---
+++ java/KT-AIVLE-CodingMasters/src/CodingTestPre2.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+public class CodingTestPre2 {
+    public static void main(String[] args)throws IOException{
+        int [][]arr = {{5, 4, 8, 7}, {7, 3, 1, 2}, {3, 8, 12, 6}, {11, 4, 5, 4}};
+
+        int [][]result = solution(arr, 4);
+
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; j < result[0].length; j++){
+                System.out.print(result[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] solution(int[][] arr, int k) {
+        int row = arr[0].length; // 세로 길이
+        int col = arr.length; // 가로 길이
+        int[][] answer = new int[row][col];
+
+        for(int i = 0; i < k; i++) {
+            int r_index = 0;
+            int c_index = 0;
+
+            if (col >= row) {
+                col = col / 2;
+                answer = new int[row][col];
+
+                for (int r = 0; r < row; r++) {
+                    for (int c = 0; c < col + 1; c += 2) {
+                        if (arr[r][c] >= arr[r][c + 1]) {
+                            answer[r_index][c_index] = arr[r][c];
+                        } else {
+                            answer[r_index][c_index] = arr[r][c + 1];
+                        }
+                        c_index++;
+                    }
+                    r_index++;
+                    c_index = 0;
+                }
+            }
+
+            else {
+                row = row / 2;
+                answer = new int[row][col];
+
+                for (int r = 0; r < row + 1; r += 2) {
+                    for (int c = 0; c < col; c++) {
+                        if (arr[r][c] <= arr[r + 1][c]) {
+                            answer[r_index][c_index] = arr[r][c];
+                        } else {
+                            answer[r_index][c_index] = arr[r + 1][c];
+                        }
+                        c_index++;
+                    }
+                    r_index++;
+                    c_index = 0;
+                }
+            }
+
+            arr = answer;
+
+        }
+        return answer;
+    }
+}
